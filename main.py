e141019e77933bc10654c1fdb14abafe3d10c328
caiosoter <caiosoter@hotmail.com>
2023-12-08 15:40:10 -0300

    Adicionando imagem de livros, filtro para titítulo e função para calculo de similaridade

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import re
 import numpy as np
 import pandas as pd
+import pickle
+import torch
 import pyarrow.parquet as pq
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -9,16 +11,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 @st.cache_data
 def loading_data(path):
     data = pq.read_table(path).to_pandas()
-    #data["authors"] = data["authors"].apply(lambda x: re.sub(r"\[|\]", "", x))
     return data
 
 @st.cache_data()
-def loading_embedding():
-    pass
+def loading_embedding_bert(path):
+    with open(path, 'rb') as file:
+        vetor_embedding_serielizado = pickle.load(file)
+    return vetor_embedding_serielizado
 
-@st.cache_resource
-def loading_model():
-    pass
 
 def limpando_titulos(text):
     text = re.sub("[^a-zA-Z0-9\s]", "", text)
@@ -29,35 +29,48 @@ def limpando_titulos(text):
     return text
 
 
-st.markdown("# Book Adviser🎈")
+def similaridade(vetor_dataframe, vetor_amostra):
+    similaridades = cosine_similarity(torch.cat(vetor_dataframe.tolist()), vetor_amostra.numpy().reshape(1, -1))
+    return similaridades
+
+
+st.markdown("# Book Advisor :book:")
 st.subheader('I would like to suggest you a new book!!')
-data = loading_data(r"data\preprocessed_data.parquet")
+data = loading_data(r"data\preprocessed_data.parquet").head().copy()
+embendding_matrix = loading_embedding_bert(r'data\vetor_embedding_serializado.pkl')
 title_input = limpando_titulos(st.sidebar.text_input(label="Write a Title", value="Dr Seuss American icon"))
 st.dataframe(data.head())
+filtro = data[data["Title_cleaned"].str.contains(r"^{}".format(title_input), regex=True)]
 
 left_column, right_column = st.columns(2)
 with st.sidebar:
     st.write(f"## What about your book?")
-    if title_input:
-        filtro = data[data["Title_cleaned"].str.contains(title_input)]
+    if title_input and not filtro["Title"].empty:
         author = filtro["authors"].tolist()[0]
         year = filtro["publishedDate"].tolist()[0]
         nome = filtro["Title"].tolist()[0]
         description = filtro["description"].tolist()[0]
+        imagem = filtro["image"].tolist()[0]
+        if imagem:
+            st.image(imagem)
         st.write(f"**Name**: {nome}")
         st.write(f"**Author:** {author}")
         st.write(f"**Published date:** {year}")
         with st.expander(label="Description"):
             st.write(f"**Description**: {description}")
-        
-        
+    else:
+        st.write("I do not have this book in my database!")
 
-with right_column:
-    if title_input:
-        filtro = data[data["Title_cleaned"].str.contains(title_input)]
-        
 
+if not filtro["Title"].empty:
+    index = filtro.index[0]
+    vetor_embebending_amostra = embendding_matrix.iloc[index][0]
+    similaridades_df = similaridade(embendding_matrix, vetor_embebending_amostra)
+    data["similaridade"] = similaridades_df
+    st.write(data)
     
+
+
 
 
 
