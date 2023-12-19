1402f4efa1e4655349899f7ef890a9e7ecf38c50
caiosoter <caiosoter@hotmail.com>
2023-12-19 14:50:50 -0300

    Modificando a estratégia de recomendação

--- main.py
+++ main.py
@@ -2,64 +2,65 @@ import streamlit as st
 import pandas as pd
 import re
 import joblib
+from io import BytesIO
+import tempfile
+from st_files_connection import FilesConnection
 import scipy.sparse as ss
 import numpy as np
 import boto3
+import logging
 from sklearn.metrics.pairwise import cosine_similarity
-import pyarrow.parquet as pq
-
-@st.cache_data
-def loading_book_map(s3):
-    #data = pq.read_table(r"data\book_id_map.parquet").to_pandas()
-    obj = s3.Bucket(st.secrets["bucket_name"]).Object("book_id_map.parquet").get()["Body"]
-    data =  pq.read_table(obj).to_pandas()
-    return data
 
 
-@st.cache_data
-def loading_books(s3):
-    #data = pq.read_table(r"data\goodreads_books.parquet").to_pandas()
-    obj = s3.Bucket(st.secrets["bucket_name"]).Object("goodreads_books.parquet").get()["Body"]
-    data = pq.read_table(obj).to_pandas()
-    return data
+def get_s3_client():
+   s3 = boto3.client('s3', 
+         aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+         aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"])
+   return s3
 
-
-@st.cache_data
-def loading_book_interactions(s3):
-    #data = pq.read_table(r"data\goodreads_interactions.parquet").to_pandas()
-    obj = s3.Bucket(st.secrets["bucket_name"]).Object("goodreads_interactions.parquet").get()["Body"]
-    data = pq.read_table(obj).to_pandas()
-    return data
-
-@st.cache_data
-def loading_model(s3):
-    #model = joblib.load(r"models\vectorizer.joblib")
-    #data_tfidf = ss.load_npz(r"data\data_tfdi.npz")
-    obj1 = s3.Bucket(st.secrets["bucket_name"]).Object("vectorizer.joblib").get()["Body"]
-    obj2 = s3.Bucket(st.secrets["bucket_name"]).Object("data_tfdi.npz").get()["Body"]
+@st.cache_resource
+def load_model_from_s3(bucket, key):
+     s3_client = get_s3_client()
+     try:
+         with tempfile.TemporaryFile() as fp:
+             s3_client.download_fileobj(Fileobj=fp, Bucket=bucket, Key=key)
+             fp.seek(0)
+             return joblib.load(fp)
+     except Exception as e:
+         raise logging.exception(e)
     
-    return obj1, obj2
+@st.cache_data
+def loading_tfdi():
+    s3_client = get_s3_client()
+    obj = s3_client.get_object(Bucket=st.secrets["bucket_name"], Key="data_tfdi.npz")["Body"].read()
+    dados = ss.load_npz(BytesIO(obj))
+    return dados
+    
+def plotar_dados(df):
+        author = df["author"].tolist()[0]
+        year = df["publishedDate"].tolist()[0]
+        nome = df["Title"].tolist()[0]
+        url = df["url"].tolist()[0]
+        imagem = df["image"].tolist()[0]
+        if imagem:
+            st.image(imagem)
+        else:
+            st.image(st.secrets["link_sem_imagem"])
+        st.write(f"**Name**: {nome}")
+        st.write(f"**Author:** {author}")
+        st.write(f"**Published date:** {year}")
+        st.write(f"**More information**: {url}")
 
-
-def connection_s3():
-    try:
-        s3 = boto3.resource(service_name=st.secrets["service_name"],
-                        region_name=st.secrets["region_name"],
-                        aws_access_key_id=st.secrets["aws_access_key_id"],
-                        aws_secret_access_key=st.secrets["aws_secret_access_key"])
-    except ConnectionError as error:
-        st.write("Connection to AWS S3 failed")
-        st.write(error)
-    return s3
-
-
+            
 
 def search_engine(title, books, tf_data, model):
+    book_copy = books.copy()
     title = re.sub("[^a-zA-Z0-9 ]", "", title.lower())
     title_transformed = model.transform([title])
     similarity = cosine_similarity(title_transformed, tf_data).flatten()
     index = np.argpartition(similarity, -10)[-10:]
-    resuts = books.loc[index].sort_values("ratings_count", ascending=False)
+    book_copy["similarites"] = similarity
+    resuts = book_copy.loc[index].sort_values("similarites", ascending=False)
     return resuts.head(5)
 
 
@@ -73,30 +74,64 @@ def recomendacao(escolha, df_books, df_book_id_map, df_interactions):
     resultado = pd.merge(resultado, df_books, how="inner", left_on="book_id_y", right_on="book_id")
     resultado = resultado[~resultado["book_id"].isin(escolha)]
     resultado["score"] = resultado["count"] * (resultado["count"]/resultado["ratings_count"])
-    resultado = resultado[resultado["count"] > 75].sort_values("score", ascending=False).head(6)
+    resultado = resultado.sort_values(["score" , "count"], ascending=[False, False]).head(6)
     return resultado
 
-s3 = connection_s3()
-df_books = loading_books(s3)
-df_interactions = loading_book_interactions(s3)
-df_map_id = loading_book_map(s3)
-model, data_tfid = loading_model(s3)
+st.markdown("# Book Advisor :book:")
+st.subheader('I would like to suggest you a new book!!')
+conn = st.connection('s3', type=FilesConnection)
+df_map_id = conn.read("databook/book_id_map.parquet", input_format="parquet")
+df_books = conn.read("databook/goodreads_books.parquet", input_format="parquet")
+model = load_model_from_s3("databook", "vectorizer.joblib")
+dados_npz = loading_tfdi()
 
-st.write(df_books.head())
-st.write(df_interactions.head())
-st.write(type(model))
-st.write(type(data_tfid))
-
-
-
-
-
-
-"""with st.sidebar:
+with st.sidebar:
     input_title = st.text_input(label="Write a title")
-    
-output_titles = search_engine(input_title, df_books, data_tfid, model)
-st.dataframe(output_titles, column_config={"url": st.column_config.LinkColumn("Book URL")})
+    resultado = search_engine(input_title, df_books, dados_npz, model)
+    existencia1 = resultado["similarites"].max() > 0.5
 
-rec = recomendacao([output_titles["book_id"].values[0]], df_books, df_map_id, df_interactions)
-st.dataframe(rec)"""
+    input_title2 = st.text_input(label="Write a second title")
+    resultado2 = search_engine(input_title2, df_books, dados_npz, model)
+    existencia2 = resultado2["similarites"].max() > 0.5
+
+    input_title3 = st.text_input(label="Write a third title")
+    resultado3 = search_engine(input_title3, df_books, dados_npz, model)
+    existencia3 = resultado3["similarites"].max() > 0.5
+
+
+if (input_title and input_title2 and input_title3) and (existencia1 and existencia2 and existencia3):
+    dados_interactions = conn.read("databook/goodreads_interactions.parquet", input_format="parquet")
+    st.write("## About your books:")
+    left, middle, right = st.columns(3, gap="large")
+    with left:
+        plotar_dados(resultado.iloc[[0]])
+    with middle:
+        plotar_dados(resultado2.iloc[[0]])
+    with right:
+        plotar_dados(resultado3.iloc[[0]])
+
+    id_escolhido1 = resultado.iloc[[0]]["book_id"].values[0]
+    id_escolhido2 = resultado2.iloc[[0]]["book_id"].values[0]
+    id_escolhido3 = resultado3.iloc[[0]]["book_id"].values[0]
+    rec = recomendacao([id_escolhido1, id_escolhido2, id_escolhido3], df_books, df_map_id, dados_interactions)
+
+    if not rec.empty:
+        st.write("## My recommendations:")
+        left, middle, right = st.columns(3, gap="large")
+        if len(rec) == 1:
+            with middle:
+                plotar_dados(rec.iloc[[0]])
+        elif len(rec) > 2:
+            with left:
+                plotar_dados(rec.iloc[[0]])
+            with middle:
+                plotar_dados(rec.iloc[[1]])
+            with right:
+                plotar_dados(rec.iloc[[2]])
+    st.write(rec)
+
+elif input_title and (resultado["similarites"].max() < 0.7):
+    st.write("## Sorry, I do not have this book in my Dataset!!")
+
+
+
