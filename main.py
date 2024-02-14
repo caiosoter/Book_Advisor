45f7dac3b5d50b489bd4f176440a28c416534ced
Caio Sóter <86934400+caiosoter@users.noreply.github.com>
2024-02-14 14:29:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ div.st-emotion-cache-1fjr796.e1f1d6gn3{{
     border-color: darkgray;
 }}
 
-.st-emotion-cache-16txtl3.eczjsme4{{
+[data-testid="stSideBarUserContent"]{{
     padding: 0rem 1.5rem;
 }}
 </style>
